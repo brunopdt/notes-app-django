6482ff69222308657624035b0337e7fd5fd1e238
bruno duarte <bruno.pduarte0@gmail.com>
2024-02-14 15:38:08 -0300

    feat: dark/ligh mode toggle

--- views/src/components/Header.tsx
+++ views/src/components/Header.tsx
@@ -1,7 +1,13 @@
 export const Header = () => {
+  const handleModeButtonClick = (): void => {
+    const body = document.querySelector('.container')
+    body?.classList.toggle('dark')
+  }
+
   return (
     <div className="app-header">
       <h1>Lista de notas</h1>
+      <button onClick={handleModeButtonClick}>NOTURNO/DIURNO</button>
     </div>
   )
 }
